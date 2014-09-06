470013fdbac86352dde19ef21f007dbc191b89f5
unknown <divyahn3@gmail.com>
2014-09-06 15:47:24 -0700

    add all pages

--- js/main.js
+++ js/main.js
@@ -1,14 +1,48 @@
-$(document).ready(function(){
-$.ajax({
-  dataType: "json",
-  url: "valentine.json",
-    success: function(data){
-    	console.log(data);
-    }, error: function(error){
-    	console.log(error, 'error');
-    }
-});
-	
+$(document).ready(function() {
+    $.ajax({
+        dataType: "json",
+        url: "valentine.json",
+        success: function(data) {
+            var valentinedata = data;
 
+
+            for (var test in valentinedata.dateNight) {
+                $("#" + test + " img").attr("src", valentinedata.dateNight[test].image);
+                $("#" + test + " img").click(clickFn(valentinedata.dateNight[test].productId));
+            }
+
+
+
+        },
+        error: function(error) {
+            console.log(error, 'error');
+        }
+    });
+
+    function clickFn(parm1) {
+        return function() {
+            $.ajax({
+                //select the service and URI
+                url: 'http://origin-api.macys.com/v4/catalog/product/' + parm1 + '?imagewidth=100&imagequality=90',
+                //set headers with your key and select json
+                headers: {
+                    'Accept': 'application/json',
+                    'X-Macys-Webservice-Client-Id': 'neohack14'
+                },
+                success: function(data) {
+                    //do something with your data
+                    var productid=["first","sec","third"];
+                    var productArr=data["product"];
+                    for(var i=0;i<productArr.length;i++){
+                    	  var imageurl="http://slimages.macys.com/is/image/MCY/products/4/optimized/"+productArr[i].productDetails.additionalImages[0].imagename+"?bgc=255,255,255&wid=100&qlt=90&layer=comp&op_sharpen=0&resMode=bicub&op_usm=0.7,1.0,0.5,0&fmt=jpeg";
+                    	  $("#"+productid[i]).html("<img src='"+imageurl+"' rel='stylesheet'><p><a href='"+productArr[i].productDetails.summary.productURL+"'>buy</a></p>");
+                    }
+
+                    $('body').append('Look at your console in developer tools!')
+                    console.log(data);
+                }
+            })
+        }
+    }
 
 });
